5f7fc9401344d254f80e80523ce5039d380084f6
jacobhorne <jacobhorne@gmail.com>
2015-10-12 07:59:42 -0400

    additions

--- js/script.js
+++ js/script.js
@@ -97,3 +97,28 @@ $( function(){
         }
     } );
 } );
+
+
+$(window).scroll(function () { 
+
+// distance from top of footer to top of document
+footertotop = ($('#footer').position().top);
+// distance user has scrolled from top, adjusted to take in height of sidebar (570 pixels inc. padding)
+scrolltop = $(document).scrollTop()+500;
+// difference between the two
+difference = scrolltop-footertotop;
+
+// if user has scrolled further than footer,
+// pull sidebar up using a negative margin
+
+if (scrolltop > footertotop) {
+
+$('#cart').css('margin-top',  0-difference);
+}
+
+else  {
+$('#cart').css('margin-top', 0);
+}
+
+
+});
